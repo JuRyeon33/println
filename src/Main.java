27e3382983aca0db22d3573d74bee657c36cd5be
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-11 11:29:07 +0900

    %d, %.2f quiz

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-         int age = 26;
-         double pi = 3.141592;
+         String name = "Alice";
+         int age = 28;
+         double height = 164.52;
 
-        System.out.printf("나이 : %d, 원주율 : %.2f", age, pi);
+        System.out.printf("Name : %s, Age : %d, Height : %.1fcm", name, age, height);
     }
 }
